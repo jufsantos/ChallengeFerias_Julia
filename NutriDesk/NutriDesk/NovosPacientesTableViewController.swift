b01a504f27d0d9291d833ddb3aaa1ed21f2cba60
Julia Santos <juliasantos@MacBook-Pro-de-Julia-2.local>
2019-07-23 11:15:25 -0300

    atualizacao do codigo

--- NutriDesk/NutriDesk/NovosPacientesTableViewController.swift
+++ NutriDesk/NutriDesk/NovosPacientesTableViewController.swift
@@ -8,47 +8,100 @@
 
 import UIKit
 
-class NovosPacientesTableViewController: UITableViewController {
+class NovosPacientesTableViewController: UITableViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var nome: UITextField!
+    @IBOutlet weak var datanasc: UITextField!
+    @IBOutlet weak var genero: UITextField!
+    @IBOutlet weak var telefone: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var cpf: UITextField!
+    @IBOutlet weak var cep: UITextField!
+    @IBOutlet weak var endereco: UITextField!
+    @IBOutlet weak var bairro: UITextField!
+    @IBOutlet weak var cidade: UITextField!
+    @IBOutlet weak var estado: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureTextFields()
+        let cellId = "cellId"
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    var celulaEstaGrande:Bool = false
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 1 && indexPath.row == 0 {
+            if celulaEstaGrande {
+                return 250
+            }
+            else {
+                return 44
+            }
+        }
+        return 44
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == 1 && indexPath.row == 0{
+            celulaEstaGrande = !celulaEstaGrande
+            tableView.beginUpdates()
+            tableView.endUpdates()
+        }
     }
 
-    // MARK: - Table view data source
+    
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        return 4
+//    }
+//
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // #warning Incomplete implementation, return the number of rows
+//        return 11
+//    }
+//
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+//
+//        cell.textLabel?.text = "Something"
+//
+//        return cell
+//    }
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
+    private func configureTextFields(){
+        nome.delegate = self
+        datanasc.delegate = self
+        genero.delegate = self
+        telefone.delegate = self
+        email.delegate = self
+        cpf.delegate = self
+        cep.delegate = self
+        endereco.delegate = self
+        bairro.delegate = self
+        cidade.delegate = self
+        estado.delegate = self
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 
     /*
     // Override to support editing the table view.
